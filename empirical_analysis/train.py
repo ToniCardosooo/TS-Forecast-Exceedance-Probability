d5d89e929af420d486b81ac80706a0a75e90c5da
António Cardoso <antoniocardosopessoal@gmail.com>
2024-08-02 00:46:17 +0100

    Corrected train file

--- empirical_analysis/train.py
+++ empirical_analysis/train.py
@@ -15,7 +15,8 @@ from utils import preprocess_dataset, train_and_predict, get_global_auc_logloss
 DATASET = M3
 GROUP = 'Monthly'
 THR_PERCENTILE = 95
-LOSS = MQLoss
+LOSS = DistributionLoss
+
 LEVEL_LIST = [70, 80, 90]
 LOSS_KWARGS = {'level': LEVEL_LIST} if LOSS == MQLoss else {'distribution': 'Normal', 'level': LEVEL_LIST, 'return_params': True}
 HORIZON = 12
@@ -44,7 +45,8 @@ if __name__ == '__main__':
     models = {}
 
     for model_class in model_classes:
-
+        
+        # Hyperparameter tunning
         config = model_class.get_default_config(h=HORIZON, backend='ray')
         config["input_size"] = LAG
         config["max_steps"] = 1500  ### train on 1500 max steps
@@ -55,14 +57,26 @@ if __name__ == '__main__':
         if model_class in [AutoNHITS, AutoMLP]:
             config["start_padding_enabled"] = True
 
-        models[model_class.__name__] = model_class(
-            h=HORIZON,
-            loss=LOSS(**LOSS_KWARGS),
-            config=config,
-            search_alg=HyperOptSearch(),
-            backend='ray',
-            num_samples=20 ### train on 20 samples
-        )
+        # DeepAR does not return parameters due to Monte Carlo sampling
+        if model_class == AutoDeepAR:
+            LOSS_KWARGS['return_params'] = False
+
+        # Model hyperparameters
+        model_kwargs = {
+            'h': HORIZON,
+            'loss': LOSS(**LOSS_KWARGS),
+            'config': config,
+            'search_alg': HyperOptSearch(),
+            'backend': 'ray',
+            'num_samples': 20 ### train on 20 samples
+        }
+
+        # DeepAR MQLoss validation loss doesnt use 3 quatile levels by default, it's needed to set them
+        if model_class == AutoDeepAR:
+            model_kwargs['valid_loss'] = MQLoss(level=LEVEL_LIST)
+        
+        # Define model
+        models[model_class.__name__] = model_class(**model_kwargs)
 
     # Train models and save predictions
     pred_df = train_and_predict(models, train_df, test_df, horizon=HORIZON, scaler=SCALER, percentile_training_levels=LEVEL_LIST, test_percentile=THR_PERCENTILE)
@@ -73,3 +87,23 @@ if __name__ == '__main__':
     smape_df.to_csv(f"smape_{dataset_name}_{THR_PERCENTILE}_{LOSS.__name__}.csv", index=False)
 
     get_global_auc_logloss(pred_df, list(models.keys())+['SeasonalNaive'], filename=f"auc_logloss_{dataset_name}_{THR_PERCENTILE}_{LOSS.__name__}.csv")
+
+
+""" 
+NOTE
+
+1. DeepAR only allows MQLoss as validation loss (all other 4 models use None, and LSTM and GRU give errors if MQLoss is used)
+    -> i think it's better to leave validation loss as None and only use MQLoss on DeepAR
+
+2. DeepAR doesnt allow return_params = True, meaning it also computed the exceedance probabilities from the quantiles
+    -> the experiments should be then
+
+        --> train with MQLoss and calculate exc. prob. with quantiles               (excludes DeepAR)
+        --> train with DistributionLoss and calculate exc. prob. with quantiles     (includes DeepAR)
+        --> train with DistributionLoss and calculate exc. prob. with distr. params (excludes DeepAR)
+        
+        ?
+
+3. Tourism and Gluonts datasets still to be included
+
+"""
