a246b0d522c846f79400e645927421b1aa4ec10f
António Cardoso <antoniocardosopessoal@gmail.com>
2024-08-07 00:46:13 +0100

    Training files

--- empirical_analysis/train.py
+++ empirical_analysis/train.py
@@ -10,14 +10,14 @@ from ray.tune.search.hyperopt import HyperOptSearch
 
 from utilsforecast.losses import smape
 
-from utils import preprocess_dataset, train_and_predict, get_global_auc_logloss
+from utils import preprocess_dataset, train_and_numerical_forecast, predict_exceedance_from_percentiles, predict_exceedance_from_params, get_global_auc_logloss
 
 DATASET = M3
 GROUP = 'Monthly'
-THR_PERCENTILE = 95
-LOSS = DistributionLoss
+THR_PERCENTILE = [90, 95, 99]
+LOSS = MQLoss
 
-LEVEL_LIST = [70, 80, 90]
+LEVEL_LIST = [80, 90, 98]
 LOSS_KWARGS = {'level': LEVEL_LIST} if LOSS == MQLoss else {'distribution': 'Normal', 'level': LEVEL_LIST, 'return_params': True}
 HORIZON = 12
 LAG = 24 
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     df['ds'] = pd.to_datetime(df['ds'])
     train_df, test_df = preprocess_dataset(df, HORIZON, THR_PERCENTILE)
 
-    experiment_id = f"{GROUP}_{THR_PERCENTILE}_{LOSS.__name__}"
+    experiment_id = f"{GROUP}_{LOSS.__name__}"
     
     try:
         os.makedirs(f"./{dataset_name}_{experiment_id}")
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     os.chdir(f"./{dataset_name}_{experiment_id}")
 
-    model_classes = [AutoMLP, AutoNHITS, AutoLSTM, AutoGRU]
+    model_classes = [AutoMLP, AutoNHITS, AutoLSTM, AutoGRU] # AutoMLP, AutoNHITS, AutoLSTM, AutoGRU
     if LOSS == DistributionLoss:
         model_classes.append(AutoDeepAR)
 
@@ -54,9 +54,10 @@ if __name__ == '__main__':
         config["max_steps"] = 1500  ### train on 1500 max steps
         config["val_check_steps"] = 300 ### 300 steps interval for validation
         config["random_seed"] = tune.randint(1, 10)
+        config["enable_checkpointing"] = True
 
         # start_padding_enabled=True for NHITS and MLP
-        if model_class in [AutoNHITS, AutoMLP]:
+        if model_class in [AutoMLP, AutoNHITS]:
             config["start_padding_enabled"] = True
 
         # DeepAR does not return parameters due to Monte Carlo sampling
@@ -80,32 +81,25 @@ if __name__ == '__main__':
         # Define model
         models[model_class.__name__] = model_class(**model_kwargs)
 
-    # Train models and save predictions
-    pred_df = train_and_predict(models, train_df, test_df, horizon=HORIZON, scaler=SCALER, percentile_training_levels=LEVEL_LIST, test_percentile=THR_PERCENTILE)
-    pred_df.to_csv(f"pred_df_{dataset_name}_{experiment_id}.csv", index=True)
+    # Train models and save numerical forecast predictions
+    forecast_df = train_and_numerical_forecast(models, train_df, test_df, horizon=HORIZON, group=GROUP, scaler=SCALER, percentile_training_levels=LEVEL_LIST)
+    forecast_df.to_csv(f"forecast_df_{dataset_name}_{experiment_id}.csv", index=False)
 
-    smape_names = [model_name+'-median' for model_name in models.keys()] + ['SeasonalNaive']
-    smape_df = smape(pred_df, models=smape_names, id_col='unique_id', target_col='y_true')
+    # Get SMAPE values
+    models_names = ['SeasonalNaive'] + list(models.keys())
+    smape_df = smape(forecast_df, models=models_names, id_col='unique_id', target_col='y_true')
     smape_df.to_csv(f"smape_{dataset_name}_{experiment_id}.csv", index=False)
 
-    get_global_auc_logloss(pred_df, list(models.keys())+['SeasonalNaive'], filename=f"auc_logloss_{dataset_name}_{experiment_id}.csv")
+    # Predict exceedance events, save them and perform AUC and Log Loss metrics on the results
+    # From percentiles
+    exceedance_percentiles_df = predict_exceedance_from_percentiles(forecast_df, test_df, models_names, THR_PERCENTILE, f"exceedance_percentiles_{experiment_id}.csv")
+    get_global_auc_logloss(exceedance_percentiles_df, test_df, models_names, THR_PERCENTILE, filename=f"auc_logloss_percentiles_{dataset_name}_{experiment_id}.csv")
+
+    # From distribution parameters
+    if "AutoDeepAR" in list(models.keys()):
+        models_names = list(models.keys()) 
+        models_names.remove("AutoDeepAR") # Does not include Seasonal Naive nor AutoDeepAR
+        exceedance_params_df = predict_exceedance_from_params(forecast_df, test_df, models_names, THR_PERCENTILE, f"exceedance_params_{experiment_id}.csv")
+        get_global_auc_logloss(exceedance_params_df, test_df, models_names, THR_PERCENTILE, filename=f"auc_logloss_params_{dataset_name}_{experiment_id}.csv")
 
 
-""" 
-NOTE
-
-1. DeepAR only allows MQLoss as validation loss (all other 4 models use None, and LSTM and GRU give errors if MQLoss is used)
-    -> i think it's better to leave validation loss as None and only use MQLoss on DeepAR
-
-2. DeepAR doesnt allow return_params = True, meaning it also computed the exceedance probabilities from the quantiles
-    -> the experiments should be then
-
-        --> train with MQLoss and calculate exc. prob. with quantiles               (excludes DeepAR)
-        --> train with DistributionLoss and calculate exc. prob. with quantiles     (includes DeepAR)
-        --> train with DistributionLoss and calculate exc. prob. with distr. params (excludes DeepAR)
-        
-        ?
-
-3. Tourism and Gluonts datasets still to be included
-
-"""
--- empirical_analysis/utils.py
+++ empirical_analysis/utils.py
@@ -7,44 +7,70 @@ from statsforecast.models import SeasonalNaive
 from statsforecast import StatsForecast
 from neuralforecast import NeuralForecast
 
-def preprocess_dataset(df, horizon, percentile):
-    # obtain percentile value based on the train data
-    df[f'{percentile}th_percentile'] = df.groupby('unique_id')['y'].transform(lambda x: x.iloc[:-horizon].quantile(percentile/100))
-    # add classification if timeseries value exceeds the percentile
-    df['y_above_percentile'] = (df['y'] >= df[f'{percentile}th_percentile']).astype(int)
+def preprocess_dataset(df, horizon, percentiles):
+    for percentile in percentiles:
+        # obtain percentile value based on the train data
+        df[f'y_percentile_{percentile}'] = df.groupby('unique_id')['y'].transform(lambda x: x.iloc[:-horizon].quantile(percentile/100))
+        # add classification if timeseries value exceeds the percentile
+        df[f'y_above_percentile_{percentile}'] = (df['y'] >= df[f'y_percentile_{percentile}']).astype(int)
+        
     # use the last "horizon" timesteps as test timesteps
     test_df = df.groupby('unique_id').tail(horizon)
     train_df = df.drop(test_df.index)
     return train_df, test_df
 
 
-def prob_and_class(row_pred, row_test, model_name, percentile):
-    # check whether MQLoss or DistributionLoss was used by verifying there isnt/is a column with "loc"
+def predict_exceedance_from_percentiles(pred_df, test_df, models_names, percentiles, filename="exceedance_percentiles.csv"):
+    exceedance_df = pd.DataFrame()
 
-    # DistributionLoss
-    if any(["loc" in col for col in row_pred.index]):
-        mean = row_pred[f'{model_name}-loc']
-        std = row_pred[f'{model_name}-scale']
-        
-        probability = 1 - norm.cdf(row_test[f'{percentile}th_percentile'], loc=mean, scale=std)
-        classification = 1 if probability >= 0.5 else 0
-        return probability, classification
-    
-    # MQLoss
-    else:
-        # x_high = mean + std * z_high
-        # x_low = mean + std * z_low
+    for percentile in percentiles:
+
         z_low, z_high = norm.ppf([1-percentile/100, percentile/100])
-
         conf_int_length = percentile - (100-percentile)
 
-        std = (row_pred[f'{model_name}-hi-{conf_int_length}'] - row_pred[f'{model_name}-lo-{conf_int_length}']) / (z_high - z_low)
-        mean = row_pred[f'{model_name}-hi-{conf_int_length}'] - z_high * std
-        
-        probability = 1 - norm.cdf(row_test[f'{percentile}th_percentile'], loc=mean, scale=std)
-        classification = 1 if probability >= 0.5 else 0
-        return probability, classification
+        for model_name in models_names:
+            model_exceedance_preds = []
 
+            for i in range(pred_df.shape[0]):
+                row_pred = pred_df.iloc[i, :]
+                row_test = test_df.iloc[i, :]
+
+                std = (row_pred[f'{model_name}-hi-{conf_int_length}'] - row_pred[f'{model_name}-lo-{conf_int_length}']) / (z_high - z_low)
+                mean = row_pred[f'{model_name}-hi-{conf_int_length}'] - z_high * std
+                
+                probability = 1 - norm.cdf(row_test[f'y_percentile_{percentile}'], loc=mean, scale=std)
+                classification = 1 if probability >= 0.5 else 0
+                model_exceedance_preds.append(classification)
+            
+            exceedance_df[f'{model_name}_{percentile}'] = model_exceedance_preds
+    
+    exceedance_df.to_csv(filename, index=False)
+    return exceedance_df
+
+
+def predict_exceedance_from_params(pred_df, test_df, models_names, percentiles, filename="exceedance_params.csv"):
+    exceedance_df = pd.DataFrame()
+
+    for percentile in percentiles:
+
+        for model_name in models_names:
+            model_exceedance_preds = []
+
+            for i in range(pred_df.shape[0]):
+                row_pred = pred_df.iloc[i, :]
+                row_test = test_df.iloc[i, :]
+
+                mean = row_pred[f'{model_name}-loc']
+                std = row_pred[f'{model_name}-scale']
+                
+                probability = 1 - norm.cdf(row_test[f'y_percentile_{percentile}'], loc=mean, scale=std)
+                classification = 1 if probability >= 0.5 else 0
+                model_exceedance_preds.append(classification)
+            
+            exceedance_df[f'{model_name}_{percentile}'] = model_exceedance_preds
+    
+    exceedance_df.to_csv(filename, index=False)
+    return exceedance_df
 
 """
 models = {
@@ -54,17 +80,17 @@ models = {
 }
 """
 
-def train_and_predict(models, train_df, test_df, horizon, scaler, percentile_training_levels, test_percentile):
+def train_and_numerical_forecast(models, train_df, test_df, horizon, group, scaler, percentile_training_levels):
     # Seasonal Naive as a baseline
-    snaive = SeasonalNaive(season_length=12)  # Quarterly -> 4
-    sf = StatsForecast(models=[snaive], freq='M')
+    season_length = 12 if group=='Monthly' else 4 # Quarterly -> 4
+    snaive = SeasonalNaive(season_length=season_length)  
+    sf = StatsForecast(models=[snaive], freq=group[0])
     pred_sf = sf.forecast(df=train_df, h=horizon, level=percentile_training_levels)
-    pred_sf['y_true'] = test_df['y'].to_list()
-    pred_sf['y_true_above_thr'] = test_df['y_above_percentile'].to_list()
-
+    
     # Train the other models
-    nf = NeuralForecast(models=list(models.values()), freq='M', local_scaler_type=scaler)
+    nf = NeuralForecast(models=list(models.values()), freq=group[0], local_scaler_type=scaler)
     nf.fit(train_df, val_size=horizon, verbose=False)
+    pred_nf = nf.predict(verbose=False)
 
     # Save best hyperparameters for each model
     if "Auto" in nf.models[0].__class__.__name__:
@@ -72,44 +98,39 @@ def train_and_predict(models, train_df, test_df, horizon, scaler, percentile_tra
             model_name = nf.models[i].__class__.__name__
             nf.models[i].results.get_dataframe().to_csv(f"hyperparams_{model_name}.csv", index=False)
 
-    pred = nf.predict(test_df, verbose=False)
+    # Merge Season Naive results with the other models results
+    pred_df = pd.merge(pred_sf, pred_nf, how='inner', on=['unique_id','ds'])
 
-    # Put all predictions together
-    # Classify the forecast as above/below the threshold
-    pred['y_true'] = test_df['y'].to_list()
-    pred['y_true_above_thr'] = test_df['y_above_percentile'].to_list()
-    pred['SeasonalNaive'] = pred_sf['SeasonalNaive']
-    
-    for model_name in ['SeasonalNaive'] + list(models.keys()):
-        above_thr_prob = []
-        above_thr_class = []
+    # Add true values to the dataframe
+    pred_df['y_true'] = test_df['y'].to_list()
+    #y_above_percentile_cols = [col for col in test_df.columns if "percentile" in col]
+    #for col in y_above_percentile_cols:
+    #    pred_df[col] = test_df[col].to_list()
 
-        for i in range(pred.shape[0]):
-            row_pred = pred.iloc[i, :] if model_name != 'SeasonalNaive' else pred_sf.iloc[i, :]
-            row_test = test_df.iloc[i, :]
-            probability, classification = prob_and_class(row_pred, row_test, model_name, test_percentile)
-            above_thr_prob.append(probability)
-            above_thr_class.append(classification)
+    # Median results columns will not be used later
+    median_cols = [col for col in pred_df.columns if "-median" in col]
+    pred_df = pred_df.drop(columns=median_cols)
+    pred_df = pred_df.reset_index()
+    return pred_df
 
-        pred[f'{model_name}_forecast_above_thr_prob'] = above_thr_prob
-        pred[f'{model_name}_forecast_above_thr'] = above_thr_class
 
-    pred['unique_id'] = pred.index
-    return pred
-
-def get_global_auc_logloss(pred_df, models_names, filename="auc_logloss.csv"):
+def get_global_auc_logloss(pred_df, test_df, models_names, test_percentiles, filename="auc_logloss.csv"):
     auc_logloss_df = []
     
-    y_true_above_thr = pred_df['y_true_above_thr'].tolist() + [0,1]
-    for model_name in models_names:
-        model_forecast_above_thr = pred_df[f'{model_name}_forecast_above_thr'].tolist() + [0,1]
-        model_auc = roc_auc_score(y_true_above_thr, model_forecast_above_thr)
-        model_logloss = log_loss(y_true_above_thr, model_forecast_above_thr)
-        auc_logloss_df.append({
-            'Model': model_name,
-            'AUC': model_auc,
-            'LogLoss': model_logloss
-        })
-    
+    for percentile in test_percentiles:
+
+        y_true_above_thr = test_df[f'y_above_percentile_{percentile}'].tolist() + [0,1]
+
+        for model_name in models_names:
+
+            model_forecast_above_thr = pred_df[f'{model_name}_{percentile}'].tolist() + [0,1]
+            model_auc = roc_auc_score(y_true_above_thr, model_forecast_above_thr)
+            model_logloss = log_loss(y_true_above_thr, model_forecast_above_thr)
+            auc_logloss_df.append({
+                'Model_Percentile': f'{model_name}_{percentile}',
+                'AUC': model_auc,
+                'LogLoss': model_logloss
+            })
+        
     auc_logloss_df = pd.DataFrame(data=auc_logloss_df)
     auc_logloss_df.to_csv(filename, index=False)
